e9f155ff52b0fc8a4b4aba88de2a439a0a8bdcaf
RinkuRenold <51430097+RinkuRenold@users.noreply.github.com>
2019-07-01 15:40:58 +0800

    Create library.go

---
+++ library.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+const (
+	rainy  = 1
+	summer = 3
+	yearly = 4
+)
+
+func main() {
+	books := [...]string{
+		"Kaanthi",
+		"wings of fire",
+		"life",
+		"Kanthi Second edition",
+	}
+	var (
+		rbooks [rainy]string
+		sbooks [summer]string
+	)
+	rbooks[0] = books[0]
+	for i := range sbooks {
+		sbooks[i] = books[i+1]
+	}
+	fmt.Printf("Rainy books  : %#q\n", rbooks)
+	fmt.Printf("Summer books : %#q\n", sbooks)
+
+}
